be9ab3ec220df727d636dce418db9cb0df108471
Jesse <jesseorhs@gmail.com>
2023-11-28 13:24:22 -0800

    feat: add postgres db server
    Added postgres db server via docker image using dbcontext class

--- src/AuctionService/Entities/Item.cs
+++ src/AuctionService/Entities/Item.cs
@@ -1,6 +1,19 @@
-﻿namespace AuctionService;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using AuctionService.Entities;
 
+namespace AuctionService;
+
+[Table("Items")]
 public class Item
 {
+    public Guid Id { get; set; }
+    public string Artist { get; set; }
+    public string Album { get; set; }
+    public int Year { get; set; }
+    public string Genre { get; set; }
+    public string ImageUrl { get; set; }
 
+    // nav properties
+    public Auction Auction { get; set; }
+    public Guid AuctionId { get; set; }
 }
